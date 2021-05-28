03dd4191351072b964f0b8c8c879163a739b1e2e
mercado-moderno <juniorsantosdvlp@gmail.com>
2021-05-27 22:03:38 -0300

    commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddProductComponent } from './component/add-product/add-product.component';
+import { ListProductsComponent } from './component/list-products/list-products.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddProductComponent,
+    ListProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
